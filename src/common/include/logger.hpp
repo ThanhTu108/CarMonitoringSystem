fd653d35a8d142cee05a932ffe53e0c268e80894
ThanhTu <thanhtu10803@gmail.com>
2026-07-19 15:13:19 +0700

    [spdlog]: add header file with include (log info, err, warn, debug)

--- src/common/include/logger.hpp
+++ src/common/include/logger.hpp
@@ -1,13 +1,63 @@
 #pragma once
+// Third-party library
 #include <fmt/format.h>
 
+#include <iostream>
+#include <source_location>
 #include <string_view>
 
 namespace spdlog {
     class logger;
 }
 
-class logger {
-    public:
-    private:
+class Logger {
+private:
+    enum class LogLevel { INFO = 0, ERROR, DEBUG, WARN };
+    void log_impl(LogLevel level, std::string_view fmt_str,
+                  fmt::format_args args, const std::source_location& loc);
+    std::shared_ptr<spdlog::logger> spd_logger;
+
+public:
+    Logger(std::string_view path_file);
+    ~Logger();
+    template <typename... Args>
+    void debug(
+        fmt::format_string<Args...> fmt, Args&&... args,
+        const std::source_location loc = std::source_location::current()) {
+        if constexpr (static_cast<int>(Logger::LogLevel::DEBUG) >=
+                      LOG_LEVEL_CMAKE) {
+            log_impl(Logger::LogLevel::DEBUG, fmt,
+                     fmt::make_format_args(args...), loc);
+        }
+    }
+    template <typename... Args>
+    void warn(
+        fmt::format_string<Args...> fmt, Args&&... args,
+        const std::source_location loc = std::source_location::current()) {
+        if constexpr (static_cast<int>(Logger::LogLevel::WARN) >=
+                      LOG_LEVEL_CMAKE) {
+            log_impl(Logger::LogLevel::WARN, fmt,
+                     fmt::make_format_args(args...), loc);
+        }
+    }
+    template <typename... Args>
+    void info(
+        fmt::format_string<Args...> fmt, Args&&... args,
+        const std::source_location loc = std::source_location::current()) {
+        if constexpr (static_cast<int>(Logger::LogLevel::INFO) >=
+                      LOG_LEVEL_CMAKE) {
+            log_impl(Logger::LogLevel::INFO, fmt,
+                     fmt::make_format_args(args...), loc);
+        }
+    }
+    template <typename... Args>
+    void error(
+        fmt::format_string<Args...> fmt, Args&&... args,
+        const std::source_location loc = std::source_location::current()) {
+        if constexpr (static_cast<int>(Logger::LogLevel::ERROR) >=
+                      LOG_LEVEL_CMAKE) {
+            log_impl(Logger::LogLevel::ERROR, fmt,
+                     fmt::make_format_args(args...), loc);
+        }
+    }
 };
